c6bda381a36d437351d86dd3329311f1793f59fa
Vincze Roland <vrolandd.public@gmail.com>
2026-06-17 16:26:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'amplify-adapter';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -16,7 +16,7 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter({ fallback: 'index.html' }),
+			adapter: adapter(),
 
 			typescript: {
 				config: (config) => ({
